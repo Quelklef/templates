38b2c8f5253d906a28c48ee8b417cc78ffe5667e
Maynard <elimaynard923@gmail.com>
2021-10-03 17:46:01 -0500

    command.nix

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+My personal template library
+
 - `porygon` - a purescript/nix template
+
+Install `command.nix` with `nix-env` to have a template-cloning command.
---
+++ command.nix
@@ -0,0 +1,52 @@
+{ pkgs ? import <nixpkgs> {}
+, command-name ? "q.init-template"
+}:
+
+let
+
+gitignoreSource =
+  let fetched = builtins.fetchGit {
+        url = "https://github.com/hercules-ci/gitignore.nix";
+        rev = "80463148cd97eebacf80ba68cf0043598f0d7438";
+      };
+  in (import fetched { inherit (pkgs) lib; }).gitignoreSource;
+
+in
+
+pkgs.writeShellScriptBin command-name ''
+  function main {
+    case "$#" in
+      0)  list-templates ;;
+      1)  init-template "$1" ;;
+      *)  fail 'Expected 0 or 1 commands' ;;
+    esac
+  }
+
+  function fail {
+    echo "$@"
+    exit 1
+  }
+
+  function list-templates {
+    echo "Run '${command-name} <template>' to clone a template"
+    echo "Available templates:"
+    for f in $(ls ${gitignoreSource ./.}); do
+      if [ -d "${./.}/$f" ]; then
+        echo "- $f"
+      fi
+    done
+  }
+
+  function init-template {
+    local tname="$1"
+
+    [ -d "${./.}/$tname" ] || fail "No template '$tname'"
+    [ -d "./$tname" ] && fail "./$tname already exists"
+
+    mkdir -p "./$tname" &&
+    cp -r "${./.}/$tname/." "./$tname" &&
+    echo "Initialized to ./$tname"
+  }
+
+  main "$@"
+''
